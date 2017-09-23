41972101fc882a626bae1177f07357861740f267
ryan <ryankawall01@gmail.com>
2017-09-22 20:14:02 -0400

    React Quiz App
    Configured package.json and webpack, created main app and
    index.html. Created states and passed to QuestionList component.

---
+++ src/components/App.jsx
@@ -0,0 +1,100 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import QuestionList from './quiz/QuestionList.jsx';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questions: [
+        {
+          id: 1,
+          text: 'What is your name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Michael'
+            },
+            {
+              id: 'b',
+              text: 'Brad'
+            },
+            {
+              id: 'c',
+              text: 'Steve'
+            }
+          ],
+          correct: 'b'
+        },
+        {
+          id: 2,
+          text: 'What is your mothers name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Sara'
+            },
+            {
+              id: 'b',
+              text: 'Sue'
+            },
+            {
+              id: 'c',
+              text: 'Donna'
+            }
+          ],
+          correct: 'c'
+        },
+        {
+          id: 3,
+          text: 'What is your fathers name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'Bobby'
+            },
+            {
+              id: 'b',
+              text: 'Harry'
+            },
+            {
+              id: 'c',
+              text: 'Wayne'
+            }
+          ],
+          correct: 'c'
+        },
+        {
+          id: 4,
+          text: 'What is your friends name?',
+          choices: [
+            {
+              id: 'a',
+              text: 'John'
+            },
+            {
+              id: 'b',
+              text: 'Paul'
+            },
+            {
+              id: 'c',
+              text: 'Jose'
+            }
+          ],
+          correct: 'a'
+        }
+      ],
+      score: 0,
+      current: 1
+    }
+  }
+  render() {
+    return(
+      <div>
+        <QuestionList {...this.state}/>
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/components/quiz/QuestionList.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class QuestionList extends Component {
+
+  render() {
+    return(
+      <div>
+        <QuestionList />
+      </div>
+    )
+  }
+}
+
+export default QuestionList
